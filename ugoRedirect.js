232cfeb298e3d91e64eeb9a21bd9f5a62a4c9a77
Glen Wilkin <glen@luthansolutions.com>
2024-07-13 14:14:56 +0100

    refactor

--- ugoRedirect.js
+++ ugoRedirect.js
@@ -1,5 +1,5 @@
 const uGoAdminLink = "https://whatevertheugoadminurlis.com";
-const url = "https://localhost:7077/GetUgoLink/";
+const uGoApiUrl = "https://localhost:7077/GetUgoLink/";
 
 window.onload = async function(){
     //We'll actually do this:
@@ -8,13 +8,13 @@ window.onload = async function(){
 
     //For testing only
     var url = new URL('https://uGO/mypage/dosomething?value=glen#myPlaceOnPage');
-    document.getElementById('myText').value = getUrlParts(url);
-    let redirectUrl = await getRedirectUrl(url);
+    document.getElementById('myText').value = this.getUrlParts(url);
+    let redirectUrl = await this.getRedirectUrl(url);
     document.getElementById('myText2').value = redirectUrl;
 }
 
 async function getRedirectUrl(url){
-    let retVal = uGoAdminLink;
+    let retVal = this.uGoAdminLink;
     //If user hasn't entered only ugo or ugo/
     if(url.pathname && url.pathname.length > 1){
         //Get what the user entered after ugo/
@@ -24,7 +24,7 @@ async function getRedirectUrl(url){
         //Get the rest of what the user entered
         const suffix = pathText.substring(pathText.indexOf('/'));
         //Try to get the original link from ugo
-        await getUgoUrl(uGoLink).then(function(result){
+        await this.getUgoUrl(uGoLink).then(function(result){
             //If we got something
             if(result){
                 //Make the correct link to redirect to
@@ -37,7 +37,7 @@ async function getRedirectUrl(url){
 
 async function getUgoUrl(uGoLink){
     try{
-        const response = await fetch(url + uGoLink);
+        const response = await fetch(this.uGoApiUrl + uGoLink);
         if(response.ok){
             return await response.text();
         }
